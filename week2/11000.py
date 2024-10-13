218aaaf386bad4d02f0f3ceb010f08ff726e1343
밍 <79096808+0321minji@users.noreply.github.com>
2024-10-13 18:13:54 +0900

    Update 11000.py

--- week2/11000.py
+++ week2/11000.py
@@ -1,8 +1,15 @@
-import sys
+import sys,heapq
 input=sys.stdin.readline
 
 n=int(input())
-time=[]
-for _ in range(n):
-    time.append(map(int.input().split()))
-print(time)
+time=[list(map(int,input().split())) for _ in range(n)]
+time.sort()
+
+result=[]
+heapq.heappush(result,time[0][1])
+
+for i in range(1,n):
+    if result[0]<=time[i][0]:
+        heapq.heappop(result)
+    heapq.heappush(result,time[i][1])
+print(len(result))    
