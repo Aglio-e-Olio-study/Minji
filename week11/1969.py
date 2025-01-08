09a387566b1920c0258b93f7aa803de75d3102be
campus3 S007 <s007@alpacoedu.onmicrosoft.com>
2025-01-09 08:37:36 +0900

    solve 1969 with python

--- week11/1969.py
+++ week11/1969.py
@@ -4,9 +4,10 @@ from collections import Counter
 
 n,m=map(int,input().split())
 dna=list(input().rstrip() for _ in range(n))
+index=list(zip(*dna))
 
-def find(dna):
-    index=list(zip(*dna))
+## s 찾기
+def find(index):
     result=''
     for i in range(m):
         temp=index[i]
@@ -14,5 +15,17 @@ def find(dna):
 
         sorting = sorted(counter.items(),key=lambda x: (-x[1], x[0]))
         result+=sorting[0][0]
-    print(result)
     return result
+
+## 계산하기
+def cal(result,index):
+    cnt=0
+    for i in range(m):
+        cnt+=(n-index[i].count(result[i]))
+    print(result)
+    print(cnt)
+    
+
+result = find(index)
+cal(result,index)
+
